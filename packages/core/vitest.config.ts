ccd922d7ac9e2c4e0af6f7738f015a7df41f7ac3
sukyeong <sukyeong@tossbank.com>
2026-02-09 21:15:05 +0900

    chore: remove unnecessary code

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
     globals: true,
-    pool: 'forks',
     sequence: {
       concurrent: false,
     },
